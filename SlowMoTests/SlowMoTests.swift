50ccc704038a812aec4bc88a7fd9c50b00493ddf
Walter Fettich <wfettich@gmail.com>
2025-08-14 17:22:23 +0300

    Initial Commit

---
+++ SlowMoTests/SlowMoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlowMoTests.swift
+//  SlowMoTests
+//
+//  Created by Walter Fettich on 14.08.2025.
+//
+
+import Testing
+@testable import SlowMo
+
+struct SlowMoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
